56844fb9978ceb0136362937d978cf22a7b25b8f
nOraKat147 <noribuying@gmail.com>
2016-02-18 11:44:03 +0000

    Done.

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,24 @@
 # Implement your procedural solution here!
+
+def even_sum(arr)
+  sum = 0
+  arr.each do |a|
+    if a % 2 == 0
+      sum+=a
+    end
+  end
+  return sum
+end
+
+def even_fibonacci_sum(int)
+  fib_seq = [1, 2]
+  last_2_sum = 3
+  while last_2_sum <= int
+    fib_seq.push(last_2_sum)
+    last_2_sum = fib_seq[-1] + fib_seq[-2]
+  end
+  p fib_seq
+  return even_sum(fib_seq)
+end
+
+p even_fibonacci_sum(7)
--- lib/oo_even_fibonacci.rb
+++ lib/oo_even_fibonacci.rb
@@ -1,1 +1,33 @@
-# Implement your object-oriented solution here!
+class EvenFibonacci
+
+
+
+  def initialize(int)
+    @int = int
+  end
+
+  def sum
+    @fib_seq = [1, 2]
+    last_2_sum = 3
+    while last_2_sum <= @int
+      @fib_seq.push(last_2_sum)
+      last_2_sum = @fib_seq[-1] + @fib_seq[-2]
+    end
+    p @fib_seq
+    return even_sum
+  end
+
+  def even_sum
+    sum = 0
+    @fib_seq.each do |a|
+      if a % 2 == 0
+        sum += a
+      end
+    end
+    return sum
+  end
+
+end #EvenFibonacci
+
+
+p EvenFibonacci.new(100).sum
